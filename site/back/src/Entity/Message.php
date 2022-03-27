0e8aeb5d61936967cf94cf77b3fd974db75f7620
Axel Pion <axel.pion@betc.com>
2022-03-27 13:28:23 +0200

    feat(db): add relationship between messages as answers

--- site/back/src/Entity/Message.php
+++ site/back/src/Entity/Message.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MessageRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
@@ -18,6 +20,18 @@ class Message
 
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'message')]
     private $user;
+
+    #[ORM\ManyToMany(targetEntity: self::class, inversedBy: 'messages')]
+    private $answer;
+
+    #[ORM\ManyToMany(targetEntity: self::class, mappedBy: 'answer')]
+    private $messages;
+
+    public function __construct()
+    {
+        $this->answer = new ArrayCollection();
+        $this->messages = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +58,57 @@ class Message
     public function setUser(?User $user): self
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getAnswer(): Collection
+    {
+        return $this->answer;
+    }
+
+    public function addAnswer(self $answer): self
+    {
+        if (!$this->answer->contains($answer)) {
+            $this->answer[] = $answer;
+        }
+
+        return $this;
+    }
+
+    public function removeAnswer(self $answer): self
+    {
+        $this->answer->removeElement($answer);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(self $message): self
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages[] = $message;
+            $message->addAnswer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(self $message): self
+    {
+        if ($this->messages->removeElement($message)) {
+            $message->removeAnswer($this);
+        }
 
         return $this;
     }
