2b2a3cc6e9e6baf77e8987884d6fd41ebe30b068
Axel Pion <axel.pion@betc.com>
2022-04-02 13:31:52 +0200

    feat(form): adding crud forms for controllers

---
+++ site/back/src/Form/BadgeType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Badge;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BadgeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('picture')
+            ->add('created_at')
+            ->add('update_at')
+            ->add('users')
+            ->add('module')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Badge::class,
+        ]);
+    }
+}
