626f19629bac60c8d0ab7bccddedcabd3da8fb50
Axel Pion <axel.pion@betc.com>
2022-04-17 15:59:05 +0200

    feat(user): edit user

--- site/back/src/Controller/UserController.php
+++ site/back/src/Controller/UserController.php
@@ -13,10 +13,8 @@ use DateTimeImmutable;
 use Doctrine\Persistence\ManagerRegistry;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Guess\Guess;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/user', name: 'user')]
@@ -53,7 +51,7 @@ class UserController extends AbstractController
         try {
             $id = $request->get('id');
             $em = $this->doctrine->getManager();
-            $user = $em->getRepository(User::class)->find($id);
+            $user = $this->userRepository->find($id);
 
             if (empty($user)) {
                 $this->status['result'] = "error";
@@ -118,11 +116,11 @@ class UserController extends AbstractController
         try {
             $em = $this->doctrine->getManager();
             $id = $request->get('id');
-            $user = $em->getRepository(User::class)->find($id);
+            $user = $this->userRepository->find($id);
 
             if (empty($user)) {
                 $this->status['result'] = "error";
-                $this->status['msg'] = "Request user not found.";
+                $this->status['msg'] = "Requested user not found.";
             } else {
                 $em->remove($user);
                 $em->flush();
@@ -138,67 +136,54 @@ class UserController extends AbstractController
 
         return new JsonResponse($response);
     }
-//    #[Route('/', name: 'app_user_index', methods: ['GET'])]
-//    public function index(UserRepository $userRepository): Response
-//    {
-//        return $this->render('user/index.html.twig', [
-//            'users' => $userRepository->findAll(),
-//        ]);
-//    }
-//
-//    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-//    public function new(Request $request, UserRepository $userRepository): Response
-//    {
-//        $user = new User();
-//        $form = $this->createForm(UserType::class, $user);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $userRepository->add($user);
-//            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-//        }
-//
-//
-//        return $this->renderForm('user/new.html.twig', [
-//            'user' => $user,
-//            'form' => $form,
-//        ]);
-//    }
-//
-//    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-//    public function show(User $user): Response
-//    {
-//        return $this->render('user/show.html.twig', [
-//            'user' => $user,
-//        ]);
-//    }
-//
-//    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-//    public function edit(Request $request, User $user, UserRepository $userRepository): Response
-//    {
-//        $form = $this->createForm(UserType::class, $user);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $userRepository->add($user);
-//            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-//        }
-//
-//        return $this->renderForm('user/edit.html.twig', [
-//            'user' => $user,
-//            'form' => $form,
-//        ]);
-//    }
-//
-//    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
-//    public function delete(Request $request, User $user, UserRepository $userRepository): Response
-//    {
-//        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-//            $userRepository->remove($user);
-//        }
-//
-//        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-//    }
+
+    #[Route('/edit', name: '_edit', methods: ['POST'], requirements: ["id" => "^[1-9]\d*$"])]
+    public function edit(Request $request): JsonResponse
+    {
+        try {
+            $em = $this->doctrine->getManager();
+            $content = json_decode($request->getContent(), true);
+            $id = $request->query->get('id');
+            $user = $this->userRepository->find($id);
+
+            if (empty($user)) {
+                $this->status['result'] = "error";
+                $this->status['msg'] = "Requested user not found.";
+            } else {
+                $form = $this->createForm(UserType::class, $user);
+
+                $role = (array_key_exists('role', $content) || !empty($content['role'])) ? $this->setUserRole($content['role']) : $this->setUserRole(1);
+                if (array_key_exists('offer', $content) || !empty($content['offer'])) $offer = $this->setUserOffer($content['offer']);
+                if (array_key_exists('badge', $content) || !empty($content['badge'])) $badge = $this->setUserBadge($content['badge']);
+
+                $content['role'] = $role->getId();
+                if (isset($badge)) $content['badge'] = $badge->getId();
+                if (isset($offer)) $content['offer'] = $offer->getId();
+
+                $request->request->add($content);
+                $form->submit($request->request->all(), true);
+
+                if ($form->isSubmitted() && $form->isValid()) {
+                    $user->setCreatedAt(new DateTimeImmutable());
+                    $em->persist($user);
+                    $em->flush();
+
+                    $this->status['msg'] = "User edited.";
+                } else if ($form->isSubmitted() && !$form->isValid()) {
+                    $this->status['result'] = "error";
+                    $this->status['msg'] = sprintf('Error in form: "%s"', $form->getErrors(true)->current()->getMessage());
+                }
+
+                $response = ['result' => $this->status['result'], 'msg' => $this->status['msg']];
+            }
+        } catch (Exception $e) {
+            $this->status['result'] = "error";
+            $response = ['result' => $this->status['result'], 'msg' => sprintf('Exception thrown : "%s"', $e->getMessage()), 'data' => []];
+        }
+
+        return new JsonResponse($response);
+    }
+
     public function setUserRole(int $data): Role
     {
         $roleRepository = $this->doctrine->getRepository(Role::class);
