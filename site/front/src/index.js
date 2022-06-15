76593128cdf50710c108dd9e4124ba0e76edac38
Axel Pion <axel.pion@betc.com>
2022-06-15 09:17:55 +0200

    fix(front): correct file path

--- site/front/src/index.js
+++ site/front/src/index.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
-import {Navbar} from "./components/Navbar/Left/Navbar";
-import {Menu} from "./components/Navbar/Top/Menu";
-
-ReactDOM.render(
-  <BrowserRouter>
-    <Navbar />
-    <div className={"menu-app"}>
-      <Menu />
-      <App />
-    </div>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
-=======
 import './index.scss';
 import Answer from './components/answer/Answer.js';
 import Badge from './components/badge/Badge.js';
@@ -130,4 +104,3 @@ const Routes = class Routing {
 const routes = new Routes();
 
 routes.run();
->>>>>>> main
