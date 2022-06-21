e3c51e2b331c7d3811b700f1bde7406ccfca4199
Axel Pion <axel.pion@betc.com>
2022-06-21 09:11:07 +0200

    feat(front): question page

--- site/front/src/components/part/Part.js
+++ site/front/src/components/part/Part.js
@@ -1,7 +1,37 @@
+import './Part.scss';
+
 const Part = class {
   constructor() {
     this.el = document.querySelector('#body');
   }
+
+  renderPart = () => (
+    `
+      <div class="details">
+        <div class="video-p">
+          <iframe width="769" height="407" src="https://www.youtube.com/embed/N9LV0BGDYIo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <p class="error-message">Vidéo d'exemple. Crédit de la vidéo : BetterStudy Formation en comptabilité suisse, <a href="https://www.youtube.com/c/betterstudy" target="_blank">lien de la chaine</a></p>
+        </div>
+        <div class="button-p">
+          <button type=button" onclick="location.href='question'">Testez vos connaissances</button>
+          <h6>Points clefs</h6>
+          <p>Adresser des e-mails et des offres spéciales aux clients peur jouer un rôle clé dans votre stratégie marketing globale, et vous permettre d’établir et de renforcer vos relations avec eux. Dans cette vidéo, nous vous expliquerons comment:
+            <ul>
+              <li>
+                développer une liste de contacts;
+              </li>
+              <li>
+                cibler un public spécifique en fonction de ses centres d’intérêt;
+              </li>
+              <li>
+                construire une relation durable avec les clients.
+              </li>
+            </ul>
+          </p>
+        </button>
+      </div>
+    `
+  );
 
   renderSelectedLink = () => {
     const courses = document.querySelector('#courses-link');
@@ -10,20 +40,23 @@ const Part = class {
 
   render = () => (
     `
-    <div class="previous-title">
-      <button type="button" class="previous-button" onclick="location.href='chapter'">
-        <i class="fa-solid fa-angle-left"></i>
-      </button>
-      <h2>Part 1</h2>
-    </div>
-    <div id="part-details">
-    </div>
+      <div class="previous-title">
+        <button type="button" class="previous-button" onclick="location.href='chapter'">
+          <i class="fa-solid fa-angle-left"></i>
+        </button>
+        <h2>Part 5: Analyse de la cible</h2>
+      </div>
+      <div id="part-details">
+      </div>
     `
   );
 
   run = () => {
     this.renderSelectedLink();
     this.el.innerHTML = this.render();
+
+    const part = document.querySelector('#part-details');
+    part.innerHTML = this.renderPart();
   };
 };
 
--- site/front/src/components/question/Question.js
+++ site/front/src/components/question/Question.js
@@ -1,11 +1,150 @@
+import './Question.scss';
+
 const Question = class {
   constructor() {
     this.el = document.querySelector('#body');
   }
 
-  render = () => ('<h2>Question</h2>');
+  renderQuestion = () => (
+    `
+      <div class="question">
+        <div class="question-titles">
+          <h6>Question 1</h6>
+          <h5>Quel facteur permet à un moteur de recherche de déterminer si votre entreprise est locale ?</h5>
+        </div>
+        <div class="separator"></div>
+        <div class="question-choices">
+          <div class="icon-border">
+            <i class="fa-solid fa-a"></i>
+          </div>
+          <input type="checkbox" id="answer-a" name="answer-a">
+          <label for="answer-a" class="answer-a">Les informations géographiques figurant sur le site Web, la qualité du contenu et l’adéquation du site Web aux mobiles.</label>
+        </div>
+        <div class="separator"></div>
+        <div class="question-choices">
+          <div class="icon-border">
+            <i class="fa-solid fa-b"></i>
+          </div>
+          <input type="checkbox" id="answer-b" name="answer-b">
+          <label for="answer-b" class="answer-b">Les informations géographiques figurant sur le site Web, la qualité du contenu et l’adéquation du site Web aux mobiles.</label>
+        </div>
+        <div class="separator"></div>
+        <div class="question-choices">
+          <div class="icon-border">
+            <i class="fa-solid fa-c"></i>
+          </div>
+          <input type="checkbox" id="answer-c" name="answer-c">
+          <label for="answer-c" class="answer-c">Les informations géographiques figurant sur le site Web, la qualité du contenu et l’adéquation du site Web aux mobiles.</label>
+        </div>
+        <div class="separator"></div>
+        <div class="question-choices">
+          <div class="icon-border">
+            <i class="fa-solid fa-d"></i>
+          </div>
+          <input type="checkbox" id="answer-d" name="answer-d">
+          <label for="answer-d" class="answer-d">Les informations géographiques figurant sur le site Web, la qualité du contenu et l’adéquation du site Web aux mobiles.</label>
+        </div>
+        <div class="separator"></div>
+      </div>
+    `
+  );
 
-  run = () => { this.el.innerHTML = this.render(); };
+  renderSelectedLink = () => {
+    const courses = document.querySelector('#courses-link');
+    courses.className = 'selected';
+  };
+
+  render = () => (
+    `
+      <div class="previous-title">
+        <button type="button" class="previous-button" onclick="location.href='chapter'">
+          <i class="fa-solid fa-angle-left"></i>
+        </button>
+      </div>
+      <div class="survey">
+        <div id="question-details">
+        </div>
+        <button type="button" class="valid-answers">Valider les réponses</button>
+      </div>
+    `
+  );
+
+  waitForElm(selector) {
+    return new Promise((resolve) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(() => {
+        if (document.querySelector(selector)) {
+          resolve(document.querySelector(selector));
+          observer.disconnect();
+        }
+        return 1;
+      });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+    });
+  }
+
+  run = () => {
+    this.el.innerHTML = this.render();
+    this.renderSelectedLink();
+
+    const questions = document.querySelector('#question-details');
+    questions.innerHTML = this.renderQuestion();
+
+    this.waitForElm('.answer-a').then(() => {
+      const answerA = document.querySelector('.answer-a');
+
+      answerA.addEventListener('click', () => {
+        if (!answerA.attributes.checked) {
+          answerA.setAttribute('checked', 'checked');
+        } else {
+          answerA.removeAttribute('checked');
+        }
+      });
+    });
+
+    this.waitForElm('.answer-b').then(() => {
+      const answerB = document.querySelector('.answer-b');
+
+      answerB.addEventListener('click', () => {
+        if (!answerB.attributes.checked) {
+          answerB.setAttribute('checked', 'checked');
+        } else {
+          answerB.removeAttribute('checked');
+        }
+      });
+    });
+
+    this.waitForElm('.answer-c').then(() => {
+      const answerC = document.querySelector('.answer-c');
+
+      answerC.addEventListener('click', () => {
+        if (!answerC.attributes.checked) {
+          answerC.setAttribute('checked', 'checked');
+        } else {
+          answerC.removeAttribute('checked');
+        }
+      });
+    });
+
+    this.waitForElm('.answer-d').then(() => {
+      const answerD = document.querySelector('.answer-d');
+
+      answerD.addEventListener('click', () => {
+        if (!answerD.attributes.checked) {
+          answerD.setAttribute('checked', 'checked');
+        } else {
+          answerD.removeAttribute('checked');
+        }
+      });
+    });
+  };
 };
 
 export default Question;
